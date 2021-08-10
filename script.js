58792b30a5cb52a81ab122c63eaf6c8b1b372757
Abdurashid88 <83313837+Abdurashid88@users.noreply.github.com>
2021-08-11 00:01:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const number = 8; 
+const firstName = "Abdurashid";
+const bool = false;
+
+const person = {
+  ism: "Abdurashid" ,
+  yosh: 29 ,
+  familiya: "Umarov",
+  uylanganmi: true,
+};
+console.log(person.ism);
+
+const arr = ["Olma", "Anor", "Behi", "Shaftoli"]
+console.log(arr[0])
+
+const malumot = [];
+malumot[0] = prompt("Ismingiz nima");
+malumot[1] = prompt("yoshingiz nechida");
+malumot[2] = prompt("Ish joyingiz");
+console.log(malumot);
+
+const isim2 = `Abdurashid`;
+console.log(isim2);
